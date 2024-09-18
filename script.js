ecfd5c5931c9b7152042f9420ba578c0f0b0810f
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-18 09:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here!
+let list = document.getElementById("infi-list");
+list.addEventListener("scroll", function() {
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		let li1 = document.createElement("li");
+		li1.innerText = "Item 11";
+		list.appendChild(li1);
 
+		let li2 = document.createElement("li");
+		li2.innerText = "Item 12";
+		list.appendChild(li2);
+	}
+})
